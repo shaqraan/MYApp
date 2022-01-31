9ca8999ba5a19495bad3d665961cdb26db40f0e2
shaqraan <shaqraan113@gmail.com>
2022-01-31 21:42:45 +0500

    Initial commit

---
+++ app/src/main/java/com/example/funal/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.funal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button b1 , b2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1=(Button) findViewById(R.id.login);
+        b2=(Button) findViewById(R.id.signup);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent s=new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(s);
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent p=new Intent(MainActivity.this,signup.class);
+                startActivity(p);
+
+
+            }
+        });
+    }
+}
